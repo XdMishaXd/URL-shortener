be019ced6e69a8f17232d37fc02a63f2c7d7b2ac
XdMishaXd <prunchakmichael306@gmail.com>
2025-07-12 15:02:38 +0300

    added handlers

---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,90 @@
+package save
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/lib/random"
+	"url-shortener/internal/storage"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+)
+
+const aliasLength = 6
+
+type Request struct {
+	URL   string `json:"url" validate:"requierd,url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Alias string `json:"alias,omitempty"`
+}
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("Failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("Failed to decode request"))
+
+			return
+		}
+
+		log.Info("Request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("Invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.ValidationError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("URL already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, resp.Error("URL already exists"))
+
+			return
+		}
+
+		log.Info("URL added", slog.Int64("id", id))
+
+		ResponseOK(w, r, alias)
+	}
+}
+
+func ResponseOK(w http.ResponseWriter, r *http.Request, alias string) {
+	render.JSON(w, r, Response{
+		Response: resp.OK(),
+		Alias:    alias,
+	})
+}
